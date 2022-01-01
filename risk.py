014865fe803455f9993a817fe5059c9e35ec762b
Jianyu Gong <jianyu0503@gmail.com>
2022-01-01 12:04:15 -0500

    加入基金数据预处理

--- risk.py
+++ risk.py
@@ -1,4 +1,5 @@
 from pyspark.sql.functions import *
+from pyspark.sql import Window
 
 @udf
 def cal_risk(ChiName, FundTypeCode1, FundTypeCode2, RiskLevel):
@@ -8,7 +9,7 @@ def cal_risk(ChiName, FundTypeCode1, FundTypeCode2, RiskLevel):
     覆盖原本的风险等级
     """
     # QDII基金
-    if FundTypeCode1 == "14" and FundTypeCode2 == "1450":
+    if FundTypeCode1 == 14 and FundTypeCode2 == 1450:
         # 商品QDII基金（除黄金QDII）
         if "原油" in ChiName or "商品" in ChiName or "通胀" in ChiName:
             return "R5"
@@ -51,3 +52,57 @@ def cal_speical_fund_risk(ChiName, SecurityCode, InitRiskLevel):
         return "R4"
     else:
         return InitRiskLevel
+
+
+def pre_process_data(df_fundarchives, df_secumain, df_fundtype, df_fundrisklevel, df_fundtypechangenew):
+    """
+    根据query来整合基金的type和名称
+    """
+    date_threshod = '2021-09-30'
+
+    # EstablishmentDate <= '2021-09-30' AND (ExpireDate IS NULL OR ExpireDate > '2021-09-30'
+    df_fundarchives = df_fundarchives.filter((col("EstablishmentDate") <= date_threshod) & ((col("ExpireDate") > date_threshod) | (col("ExpireDate").isNull())))
+
+    # SecuCategory IN (8,13) and ListedState IN (1,9)
+    df_secumain = df_secumain.filter(((col("SecuCategory") == 8) | (col("SecuCategory") == 13)) & ((col("ListedState") == 1) | (col("ListedState") == 9)))
+
+    # dbo.MFE_FundTypeChangeNew WHERE StartDate <= '2021-09-30' AND FundType NOT LIKE '18%'
+    df_fundtypechangenew = df_fundtypechangenew.filter((col("StartDate") <= date_threshod) & (~(col("FundType").startswith("18"))))
+    windowSpec = Window.partitionBy("InnerCode").orderBy(desc("StartDate"))
+    df_fundtypechangenew = df_fundtypechangenew.withColumn("row_num", row_number().over(windowSpec))
+    df_fundtypechangenew = df_fundtypechangenew.filter(col("row_num") == 1).select(col("InnerCode"), col("FundType").alias("FundTypeCode3"))
+
+    # fund type name by joining three levels
+    df_fundtype_level_1 = df_fundtype.filter((col("Level") == 1) & (col("IfExecuted") == 1))
+    df_fundtype_level_2 = df_fundtype.filter((col("Level") == 2) & (col("IfExecuted") == 1))
+    df_fundtype_level_3 = df_fundtype.filter((col("Level") == 3) & (col("IfExecuted") == 1))
+    df_fundtype = df_fundtype_level_3.alias("l3").join(df_fundtype_level_2.alias("l2"), col("l2.FundTypeCode") == col("l3.FNodeCode"), "left") \
+                                                 .join(df_fundtype_level_1.alias("l1"),col("l1.FundTypeCode") == col("l2.FNodeCode"), "left") \
+                                                 .select(col("l1.FundTypeCode").alias("FundTypeCode1"),
+                                                         col("l1.FundTypeName").alias("FundTypeName1"),
+                                                         col("l2.FundTypeCode").alias("FundTypeCode2"),
+                                                         col("l2.FundTypeName").alias("FundTypeName2"),
+                                                         col("l3.FundTypeCode").alias("FundTypeCode3"),
+                                                         col("l3.FundTypeName").alias("FundTypeName3"))
+                                    
+    # WHERE BeginDate <= '2021-09-30'
+    df_fundrisklevel = df_fundrisklevel.filter(col("BeginDate") <= date_threshod)
+    windowSpec = Window.partitionBy("InnerCode").orderBy(desc("BeginDate"))
+    df_fundrisklevel = df_fundrisklevel.withColumn("row_num", row_number().over(windowSpec))\
+                                       .withColumn("OfficialRiskLevel", when(col("RiskLevel") == 1, "R1")
+                                                                       .when(col("RiskLevel") == 2, "R2")
+                                                                       .when(col("RiskLevel") == 3, "R3")
+                                                                       .when(col("RiskLevel") == 4, "R4")
+                                                                       .when(col("RiskLevel") == 5, "R5")
+                                                                       .otherwise(None))
+
+    df_fundrisklevel = df_fundrisklevel.filter(col("row_num") == 1) \
+                                       .select(col("InnerCode"), 
+                                               col("OfficialRiskLevel"))
+
+    df_master = df_fundarchives.join(df_secumain, ["InnerCode"], "left") \
+                               .join(df_fundtypechangenew, ["InnerCode"], "left") \
+                               .join(df_fundtype, ["FundTypeCode3"], "left") \
+                               .join(df_fundrisklevel, ["InnerCode"], "left")
+
+    return df_master
