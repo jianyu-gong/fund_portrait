456bfcf3704759c342baada8bf5afccdefc96268
Jianyu Gong <jianyu0503@gmail.com>
2022-01-01 13:11:51 -0500

    更改导出方式

--- fund_label.py
+++ fund_label.py
@@ -1,11 +1,14 @@
-from risk import cal_risk, cal_speical_fund_risk, pre_process_data
+from risk import pre_process_data, pre_fund_risk_calc
 
 class Fund_Label(object):
 
     def fund_risk_estimate(self, df_fundarchives, df_secumain, df_fundtype, df_fundrisklevel, df_fundtypechangenew, risk_mapping):
-
+        # 预处理
+        print("正在预处理数据")
         master_df = pre_process_data(df_fundarchives, df_secumain, df_fundtype, df_fundrisklevel, df_fundtypechangenew)
-        master_df = master_df.join(risk_mapping, ["FundTypeName1", "FundTypeName2", "FundTypeName3"], "left")
-        master_df = master_df.withColumn("InitRiskLevel", cal_risk("ChiName", "FundTypeCode1", "FundTypeCode2", "RiskLevel"))
-        master_df = master_df.withColumn("InitRiskLevel", cal_speical_fund_risk("ChiName", "SecurityCode", "InitRiskLevel"))
-        master_df.toPandas().to_csv("result.csv", header=True)
+        # 事前风险计算
+        print("正在计算事前风险")
+        master_df = pre_fund_risk_calc(master_df, risk_mapping)
+        print("正在导出数据")
+        master_df.coalesce(1).write.csv("result_spark",header=True) 
+        # master_df.show()
