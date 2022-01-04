20001000ebbcbed78c27a0eca3ba109b7fe69857
Jianyu Gong <jianyu0503@gmail.com>
2022-01-04 13:49:51 -0500

    更改字段名称并更新新的事前风险逻辑

--- risk.py
+++ risk.py
@@ -1,5 +1,6 @@
 from pyspark.sql.functions import *
 from pyspark.sql import Window
+from datetime import datetime
 
 @udf
 def cal_risk(ChiName, FundTypeCode1, FundTypeCode2, RiskLevel, ShareProperties):
@@ -12,44 +13,48 @@ def cal_risk(ChiName, FundTypeCode1, FundTypeCode2, RiskLevel, ShareProperties):
     if FundTypeCode1 == 15:
         return "R3"
 
-    # 找到所有新分类
-    elif RiskLevel is None:
         # QDII基金
-        if FundTypeCode1 == 14 and FundTypeCode2 == 1450:
-            # 商品QDII基金（除黄金QDII）
-            if "原油" in ChiName or "商品" in ChiName or "通胀" in ChiName:
-                return "R5-商品QDII基金（除黄金QDII）"
-            # 黄金QDII基金
-            elif "黄金" in ChiName:
-                return "R3-黄金QDII基金"
-            else:
-                return RiskLevel
-       
-       # 商品基金新分类    
-        elif FundTypeCode1 == 17:
-            if "期货" in ChiName:
-                return "R5-商品基金（除黄金基金）"
-            elif "黄金" in ChiName or "上海金" in ChiName:
-                return "R3-黄金基金"
-            else:
-                return RiskLevel
+    elif FundTypeCode1 == 14 and FundTypeCode2 == 1450:
+        # 商品QDII基金（除黄金QDII）
+        if "原油" in ChiName or "商品" in ChiName or "通胀" in ChiName:
+            return "R5-QDII基金-商品QDII基金-商品QDII基金（除黄金QDII）"
+        # 黄金QDII基金
+        elif "黄金" in ChiName:
+            return "R3-QDII基金-商品QDII基金-黄金QDII基金"
+        else:
+            return RiskLevel
+    
+    # 商品基金新分类    
+    elif FundTypeCode1 == 17:
+        if ChiName is None:
+            return RiskLevel
+        elif "期货" in ChiName:
+            return "R5-商品基金-商品基金-商品基金（除黄金基金）"
+        elif "黄金" in ChiName or "上海金" in ChiName:
+            return "R3-商品基金-商品基金-黄金基金"
+        else:
+            return RiskLevel
 
-        # 债券基金新分类
-        elif FundTypeCode1 == 12:
-            if "可转换债券" in ChiName and "分级" in ChiName and ShareProperties == 1:
-                return "R3-可转债分级子基金（优先份额）"
+    # 债券基金新分类
+    elif FundTypeCode1 == 12:
+        if ChiName is None:
+            return RiskLevel
+        elif "可转换债券" in ChiName and "分级" in ChiName and ShareProperties == 1:
+            return "R3-债券基金-债券分级子基金-可转债分级子基金（优先份额）"
 
-            elif "可转换债券" in ChiName and "分级" in ChiName and ShareProperties == 2:
-                return "R5-可转债分级子基金（进取份额）"
+        elif "可转换债券" in ChiName and "分级" in ChiName and ShareProperties == 2:
+            return "R5-债券基金-债券分级子基金-可转债分级子基金（进取份额）"
 
-            elif "可转换债券" not in ChiName and "分级" in ChiName and ShareProperties == 1:
-                return "R3-普通债券分级子基金（优先份额）"
-            
-            elif "可转换债券" not in ChiName and "分级" in ChiName and ShareProperties == 2:
-                return "R4-普通债券分级子基金（进取份额）"
+        elif "可转换债券" not in ChiName and "分级" in ChiName and ShareProperties == 1:
+            return "R3-债券基金-债券分级子基金-普通债券分级子基金（优先份额）"
+        
+        elif "可转换债券" not in ChiName and "分级" in ChiName and ShareProperties == 2:
+            return "R4-债券基金-债券分级子基金-普通债券分级子基金（进取份额）"
 
-            elif FundTypeCode2 == 1220 and "可转换债券" in ChiName:
-                return "R3-可转债指数基金"
+        elif FundTypeCode2 == 1220 and "可转换债券" in ChiName:
+            return "R3-债券基金-指数型债券基金-可转债指数基金"
+        else:
+            return RiskLevel
 
     # 不是新分类保持配置中的分类
     else:
@@ -80,15 +85,15 @@ def pre_process_data(df_fundarchives, df_secumain, df_fundtype, df_fundrisklevel
     """
     根据query来整合基金的type和名称
     """
-    date_threshod = '2021-09-30'
+    date_threshod = datetime.now().strftime('%Y-%m-%d')
 
-    # EstablishmentDate <= '2021-09-30' AND (ExpireDate IS NULL OR ExpireDate > '2021-09-30'
+    # EstablishmentDate <= date_threshod AND (ExpireDate IS NULL OR ExpireDate > date_threshod
     df_fundarchives = df_fundarchives.filter((col("EstablishmentDate") <= date_threshod) & ((col("ExpireDate") > date_threshod) | (col("ExpireDate").isNull())))
 
     # SecuCategory IN (8,13) and ListedState IN (1,9)
     df_secumain = df_secumain.filter(((col("SecuCategory") == 8) | (col("SecuCategory") == 13)) & ((col("ListedState") == 1) | (col("ListedState") == 9)))
 
-    # dbo.MFE_FundTypeChangeNew WHERE StartDate <= '2021-09-30' AND FundType NOT LIKE '18%'
+    # dbo.MFE_FundTypeChangeNew WHERE StartDate <= date_threshod AND FundType NOT LIKE '18%'
     df_fundtypechangenew = df_fundtypechangenew.filter((col("StartDate") <= date_threshod) & (~(col("FundType").startswith("18"))))
     windowSpec = Window.partitionBy("InnerCode").orderBy(desc("StartDate"))
     df_fundtypechangenew = df_fundtypechangenew.withColumn("row_num", row_number().over(windowSpec))
@@ -107,7 +112,7 @@ def pre_process_data(df_fundarchives, df_secumain, df_fundtype, df_fundrisklevel
                                                          col("l3.FundTypeCode").alias("FundTypeCode3"),
                                                          col("l3.FundTypeName").alias("FundTypeName3"))
                                     
-    # WHERE BeginDate <= '2021-09-30'
+    # WHERE BeginDate <= date_threshod
     df_fundrisklevel = df_fundrisklevel.filter(col("BeginDate") <= date_threshod)
     windowSpec = Window.partitionBy("InnerCode").orderBy(desc("BeginDate"))
     df_fundrisklevel = df_fundrisklevel.withColumn("row_num", row_number().over(windowSpec))\
@@ -138,6 +143,20 @@ def pre_fund_risk_calc(df_master, risk_mapping):
     df_master = df_master.join(risk_mapping, ["FundTypeName1", "FundTypeName2", "FundTypeName3"], "left")
     # 一般基金计算
     df_master = df_master.withColumn("InitRiskLevel", cal_risk("ChiName", "FundTypeCode1", "FundTypeCode2", "RiskLevel", "ShareProperties"))
+    # 重新定义1,2,3级分类名称
+    df_master = df_master.withColumn("FundTypeName3", when(col("InitRiskLevel").contains("-"), split(col("InitRiskLevel"), "-").getItem(3))
+                                                     .otherwise(col("FundTypeName3")))\
+                         .withColumn("FundTypeName2", when(col("InitRiskLevel").contains("-"), split(col("InitRiskLevel"), "-").getItem(2))
+                                                     .otherwise(col("FundTypeName2")))\
+                         .withColumn("FundTypeName1", when(col("InitRiskLevel").contains("-"), split(col("InitRiskLevel"), "-").getItem(1))
+                                                     .otherwise(col("FundTypeName1")))\
+                         .withColumn("InitRiskLevel", when(col("InitRiskLevel").contains("-"), split(col("InitRiskLevel"), "-").getItem(0))
+                                                     .otherwise(col("InitRiskLevel")))\
+    # 将2级名称中的指数替换成指数型，债券基金替换成债券基金（不含封闭式） 
+    df_master = df_master.withColumn("FundTypeName2", when(col("FundTypeName2").contains("指数型"), regexp_replace(df_master.FundTypeName2,'指数型','指数'))
+                                                     .otherwise(col("FundTypeName2")))\
+                         .withColumn("FundTypeName1", when(col("FundTypeName1").contains("债券基金"), regexp_replace(df_master.FundTypeName1,'债券基金','债券基金（不含封闭式）'))
+                                                     .otherwise(col("FundTypeName1")))
     # 特殊基金计算
     df_master = df_master.withColumn("InitRiskLevel", cal_speical_fund_risk("ChiName", "SecurityCode", "InitRiskLevel"))
 
